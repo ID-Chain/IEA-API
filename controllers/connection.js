94972d2f82b26b7463cd87c65c20f971b18d95c9
Ömer Ilhan <malik@mailbox.tu-berlin.de>
2018-05-30 19:01:05 +0200

    fix setting endpoint for did in create connectionOffer

--- controllers/connection.js
+++ controllers/connection.js
@@ -16,20 +16,32 @@ module.exports = {
   create: wrap(async (req, res, next) => {
     log.debug('connection controller create');
     const [fromToDid, fromToKey] = await req.wallet.createDid();
-    log.info('fromToDid: %s, fromToKey: %s', fromToDid, fromToKey);
+    /* log.debug('fromToDid: %s, fromToKey: %s', fromToDid, fromToKey);
     await indy.setEndpointForDid(req.wallet.handle, fromToDid,
       process.env.APP_ENDPOINT, fromToKey);
+    const [recipient, endpointKey] = await indy.getEndpointForDid(
+      req.wallet.handle, pool.handle, fromToDid);
+    log.debug('endpoint %s and key %s', recipient, endpointKey);*/
     const nymRequest = await indy.buildNymRequest(
       req.wallet.issuerDid, fromToDid, fromToKey);
-    log.info('nymRequest: %s', nymRequest);
+    log.debug(nymRequest);
     const nymResult = await indy.signAndSubmitRequest(
       pool.handle, req.wallet.handle, req.wallet.issuerDid, nymRequest);
-    log.info('nymResult: %s', nymResult);
+    log.debug(nymResult);
+    // const endpoint = {endpoint: {ha: process.env.APP_ENDPOINT, verkey: fromToKey}};
+    const endpoint = {endpoint: {ha: '127.0.0.1:8000'}};
+    const attribRequest = await indy.buildAttribRequest(
+      fromToDid, fromToDid, null, endpoint, null);
+    log.debug(attribRequest);
+    const attribResult = await indy.signAndSubmitRequest(
+      pool.handle, req.wallet.handle, fromToDid, attribRequest);
+    log.debug('attribResult');
+    log.debug(attribResult);
     let connectionOffer = new ConnectionOffer({
       issuerWallet: req.wallet,
       issuerDid: fromToDid,
     });
-    log.info('connectionoffer: %s', connectionOffer);
+    log.debug(connectionOffer);
     connectionOffer = await connectionOffer.save();
     next(new APIResult(200, {
       did: connectionOffer.issuerDid,
