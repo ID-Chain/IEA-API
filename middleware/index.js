f7b241809b347abcab38ba3bb46c4a12798e3526
Ömer Ilhan <malik@mailbox.tu-berlin.de>
2018-06-21 16:38:11 +0200

    enable cors support

--- middleware/index.js
+++ middleware/index.js
@@ -1,12 +1,15 @@
 
-const logMiddleware = require('../log').middleware;
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const logMiddleware = require('../log').middleware;
 const validation = require('./validate');
 const notFound = require('./404');
 const {resultMiddleware, errorMiddleware} = require('./result');
 
 module.exports = {
   before: [
+    cors(),
     logMiddleware,
     bodyParser.json(),
     validation,
