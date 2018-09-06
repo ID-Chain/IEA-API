06e224aa88750cc923acaf8eb526763de08d4d7d
Ömer Ilhan <malik@mailbox.tu-berlin.de>
2018-09-06 14:44:09 +0200

    require models at start, thereby ensuring their availability

---
+++ models/index.js
@@ -0,0 +1,29 @@
+/**
+ * IDChain Agent REST API
+ * Models
+ */
+
+const db = require('../db');
+const User = require('./user');
+const Wallet = require('./wallet');
+const Message = require('./message');
+const ConnectionOffer = require('./connectionoffer');
+const Schema = require('./credentialschema');
+const CredentialOffer = require('./credentialoffer');
+const CredentialRequest = require('./credentialreq');
+const CredentialDefinition = require('./credentialdef');
+const Credential = require('./credential');
+const ProofRequest = require('./proofreq');
+
+module.exports = {
+    User,
+    Wallet,
+    Message,
+    ConnectionOffer,
+    Schema,
+    CredentialOffer,
+    CredentialRequest,
+    CredentialDefinition,
+    Credential,
+    ProofRequest
+};
