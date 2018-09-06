e5fd64460db6d09a360c1286b4c36c070ab8db59
Ömer Ilhan <malik@mailbox.tu-berlin.de>
2018-09-06 14:57:08 +0200

    expand message model
    - add senderDid, recipientDid, and meta fields
    - add convenience methods for finding and storing messages

--- models/message.js
+++ models/message.js
@@ -1,4 +1,5 @@
 const Mongoose = require('../db');
+const MessageTypes = require('../lib').message.messageTypes;
 
 const schema = new Mongoose.Schema({
     wallet: {
@@ -16,10 +17,17 @@ const schema = new Mongoose.Schema({
         type: String,
         required: true
     },
+    senderDid: {
+        type: String
+    },
+    recipientDid: {
+        type: String
+    },
     message: {
         type: Mongoose.Schema.Types.Mixed,
         required: true
-    }
+    },
+    meta: {}
 });
 
 schema.set('toJSON', {
@@ -30,12 +38,71 @@ schema.set('toJSON', {
     }
 });
 
-schema.statics.store = function(walletId, messageId, messageType, message) {
+/**
+ * Find ConnectionOffer belonging to wallet by Id
+ * @param {string} id
+ * @param {Wallet} wallet
+ * @return {Query} Mongoose Query Object
+ */
+schema.statics.findConnectionOfferById = function findConnectionOfferById(id, wallet) {
+    return this.findTypeById(id, wallet, MessageTypes.CONNECTIONOFFER);
+};
+
+/**
+ * Find ConnectionRequest belonging to wallet by Id
+ * @param {string} id
+ * @param {Wallet} wallet
+ * @return {Query} Mongoose Query Object
+ */
+schema.statics.findConnectionRequestById = function findConnectionRequestById(id, wallet) {
+    return this.findTypeById(id, wallet, MessageTypes.CONNECTIONREQUEST);
+};
+
+/**
+ * Find ConnectionResponse belonging to wallet by Id
+ * @param {string} id
+ * @param {Wallet} wallet
+ * @return {Query} Mongoose Query Object
+ */
+schema.statics.findConnectionResponseById = function findConnectionResponseById(id, wallet) {
+    return this.findTypeById(id, wallet, MessageTypes.CONNECTIONRESPONSE);
+};
+
+/**
+ * Find ConnectionOffer belonging to wallet by Id
+ * @param {string} id
+ * @param {Wallet} wallet
+ * @param {string} type
+ * @return {Query} Mongoose Query Object
+ */
+schema.statics.findTypeById = function findTypeById(id, wallet, type) {
+    return this.findOne({
+        _id: id,
+        wallet: wallet.id,
+        type: type
+    });
+};
+
+/**
+ * Create new Message and save
+ * @param {string} walletId
+ * @param {string} messageId
+ * @param {string} messageType
+ * @param {string} senderDid
+ * @param {string} recipientDid
+ * @param {Object} message
+ * @param {Object} meta
+ * @return {Promise<Message>}
+ */
+schema.statics.store = function store(walletId, messageId, messageType, senderDid, recipientDid, message, meta) {
     return new this({
         wallet: walletId,
         messageId: messageId,
         type: messageType,
-        message: message
+        senderDid: senderDid,
+        recipientDid: recipientDid,
+        message: message,
+        meta: meta
     }).save();
 };
 
